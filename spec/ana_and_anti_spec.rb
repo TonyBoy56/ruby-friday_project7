ecc2a6d2cf16082a2689901b6762439f4a93794e
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-08 15:26:58 -0700

    ensure the fourth test passes, successfully handling inputs without actual words.

--- spec/ana_and_anti_spec.rb
+++ spec/ana_and_anti_spec.rb
@@ -20,6 +20,6 @@ describe ("#ana_and_anti") do
 
   it('the program will produce an error statement if there are no actual word inputs') do
     theirWords = Word.new('spd', 'tnl')
-    expect(theirWords.anagram()).to(eq(true))
+    expect(theirWords.anagram()).to(eq("Please input an actual word!"))
   end
 end
