dd1eec45170d97d7407d13b8016c7798876f1dad
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-09 16:44:00 -0700

    refactor constructor and methods to split received inputs into character arrays. Utilize those charArrs in written methods. Begin writing new method for examination of separate words.

--- lib/ana_and_anti.rb
+++ lib/ana_and_anti.rb
@@ -1,19 +1,19 @@
 class Word
   def initialize(firstWord, secondWord)
-    @firstWord = firstWord.downcase.chars
-    puts @firstWord
-    @secondWord = secondWord.downcase.chars
-    puts @secondWord
+    @firstWord = firstWord.downcase
+    @charArr1 = @firstWord.chars
+    @secondWord = secondWord.downcase
+    @charArr2 = @secondWord.chars
   end
 
   def vowel_checker()
     vowels = ['a', 'e', 'i', 'o', 'u', 'y']
     i = 0
     j = 0
-    @firstWord.each { |letter| 
+    @charArr1.each { |letter| 
       vowels.each { |vowel| 
         (letter.include?(vowel) ? i += 1 : i += 0)}}
-    @secondWord.each { |letter| 
+    @charArr2.each { |letter| 
       vowels.each { |vowel| 
         (letter.include?(vowel) ? j += 1 : j += 0)}}
     
@@ -24,16 +24,19 @@ class Word
     end
   end
 
+  def multipleWords()
+    # if (@firstWord.include?(" " || "," || ".") || @secondWord.include?(" " || "," || "."))
+    #   "We made it into the punctuation check!"
+    # end
+  end
+
   def anagram()
+    vowel_checker()
     if vowel_checker()
-      punctuation = 
-      # special = "?<>',?[]}{=-)(*&^%$#`~{}"
-      # regex = /[#{special.gsub(/./){|char| "\\#{char}"}}]/
-      if (@firstWord.any? {|letter| @secondWord.include?(letter)} == false)
+      punctuation = [" ", ",", "."]
+      if (@charArr1.any? {|letter| @charArr2.include?(letter)} == false)
         "This is an antigram"
-      elsif (@firstWord.include?(" " || "," || ".") || @secondWord.include?(" " || "," || "."))
-        "We made it into the punctuation check!"
-      elsif (@firstWord.length == @secondWord.length)
+      elsif (@charArr1.length == @charArr2.length)
         "These words are an anagram"
       end
     else 
