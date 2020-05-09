80708e8a5ba5ef28623318df052c7c04c0e5b38f
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-09 12:30:11 -0700

    remove .gsub method to make use of new elsif branch checking for punctuaion per input.

--- lib/ana_and_anti.rb
+++ lib/ana_and_anti.rb
@@ -1,15 +1,21 @@
 class Word
   def initialize(firstWord, secondWord)
-    @firstWord = firstWord.downcase.gsub(/[^a-z]/, '').chars
-    @secondWord = secondWord.downcase.gsub(/[^a-z]/, '').chars
+    @firstWord = firstWord.downcase.chars
+    puts @firstWord
+    @secondWord = secondWord.downcase.chars
+    puts @secondWord
   end
 
   def vowel_checker()
     vowels = ['a', 'e', 'i', 'o', 'u', 'y']
     i = 0
     j = 0
-    @firstWord.each { |letter| vowels.each { |vowel| (letter.include?(vowel) ? i += 1 : i += 0)}}
-    @secondWord.each { |letter| vowels.each { |vowel| (letter.include?(vowel) ? j += 1 : j += 0)}}
+    @firstWord.each { |letter| 
+      vowels.each { |vowel| 
+        (letter.include?(vowel) ? i += 1 : i += 0)}}
+    @secondWord.each { |letter| 
+      vowels.each { |vowel| 
+        (letter.include?(vowel) ? j += 1 : j += 0)}}
     
     if i >= 1 && j >=1 
       return true
@@ -20,8 +26,11 @@ class Word
 
   def anagram()
     if vowel_checker()
+      punctuation = " ", ",", "."
       if (@firstWord.any? {|letter| @secondWord.include?(letter)} == false)
         "This is an antigram"
+      elsif (@firstWord.include?(punctuation) || @secondWord.include?(punctuation))
+        puts "We made it to the punctuation check!"
       elsif (@firstWord.length == @secondWord.length)
         "These words are an anagram"
       end
