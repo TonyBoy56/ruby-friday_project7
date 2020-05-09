af429825f113b3ce5103516c5d9ff2075af44e6e
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-09 12:33:51 -0700

    punctuation test fails. Refactor to ensure entry into punctuation elsif.

--- spec/ana_and_anti_spec.rb
+++ spec/ana_and_anti_spec.rb
@@ -3,31 +3,33 @@ require('ana_and_anti')
 require('pry')
 
 describe ("#ana_and_anti") do
-  it('the program checks for vowels in the two words') do
-    theirWords = Word.new('tea', 'eat')
-    expect(theirWords.vowel_checker()).to(eq(true))
-  end
+  # it('the program checks for vowels in the two words') do
+  #   theirWords = Word.new('tea', 'eat')
+  #   expect(theirWords.vowel_checker()).to(eq(true))
+  # end
   
-  it('the program will return a confirmation if the words are anagrams') do
-    theirWords = Word.new('tea', 'eat')
-    expect(theirWords.anagram()).to(eq("These words are an anagram"))
-  end
+  # it('the program will return a confirmation if the words are anagrams') do
+  #   theirWords = Word.new('tea', 'eat')
+  #   expect(theirWords.anagram()).to(eq("These words are an anagram"))
+  # end
 
-  it('the program will return a confirmation if the words are ANTIGRAMS') do
-    theirWords = Word.new('sup', 'hello')
-    expect(theirWords.anagram()).to(eq("This is an antigram"))
-  end
+  # it('the program will return a confirmation if the words are ANTIGRAMS') do
+  #   theirWords = Word.new('sup', 'hello')
+  #   expect(theirWords.anagram()).to(eq("This is an antigram"))
+  # end
 
-  it('the program will produce an error statement if there are no actual word inputs') do
-    theirWords = Word.new('spd', 'tnl')
-    expect(theirWords.anagram()).to(eq("Please input an actual word!"))
-  end
+  # it('the program will produce an error statement if there are no actual word inputs') do
+  #   theirWords = Word.new('spd', 'tnl')
+  #   expect(theirWords.anagram()).to(eq("Please input an actual word!"))
+  # end
 
-  it('the program will check that different cases do not effect anagram check') do
-    theirWords = Word.new('Tea', 'Eat')
-    expect(theirWords.anagram()).to(eq("These words are an anagram"))
-  end
+  # it('the program will check that different cases do not effect anagram check') do
+  #   theirWords = Word.new('Tea', 'Eat')
+  #   expect(theirWords.anagram()).to(eq("These words are an anagram"))
+  # end
   
   it('the program will eliminate spaces and punctuation should the user input more than one word per input, accounting for multiple anagram/antigrams.') do
-    ## To Be completed by the end of the weekend ##
+    theirWords = Word.new('tea, stop', 'eat, pots')
+    expect(theirWords.anagram()).to(eq(false))
+  end
 end
