80989646d80726867b4d33300ebdbedccc9b2a73
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-10 17:58:29 -0700

    remove all puts and any comments in the document.

--- lib/ana_and_anti.rb
+++ lib/ana_and_anti.rb
@@ -2,7 +2,6 @@ class Word
   def initialize(firstWord, secondWord)
     @firstWord = firstWord.downcase
     @charArr1 = @firstWord.chars
-
     @secondWord = secondWord.downcase
     @charArr2 = @secondWord.chars
   end
@@ -32,17 +31,16 @@ class Word
       wordsArr1.each { |word1|  
         ch1 = word1.chars
         wordsArr2.each { |word2|
-          puts "working on ...", word1, word2
           @charArr1 = ch1
           @charArr2 = word2.chars
-          puts "This is what charArr1 contains", @charArr1
-          puts "This is what charArr2 contains", @charArr2
           anagram()
-          
           }}
     elsif (wordsArr2.length > wordsArr1.length)
       wordsArr2.each { |word2| 
-        wordsArr1.each { |word1| 
+        ch2 = word2.chars
+        wordsArr1.each { |word1|
+          @charArr2 = ch2
+          @charArr1 = word1.chars 
           anagram()
           }}
     end
@@ -50,18 +48,11 @@ class Word
 
   def anagram()
     vowel_checker()
-    puts "did we make it?"
     if vowel_checker()
-      puts "yeah?"
-      puts @charArr1, @charArr2
       if (@charArr1.any? {|letter| @charArr2.include?(letter)} == false)
-      # Insert the actual words that are being compared against here. Find the proper syntax #  
         "This is an antigram"
       elsif (@charArr1.length == @charArr2.length)
-        # Insert the actual words that are being compared against here. Find the proper syntax #
         "These words are an anagram"
-      elsif (@charArr1.length != @charArr2.length)
-        "error error error"
       end
     else 
       "Please input an actual word!"
