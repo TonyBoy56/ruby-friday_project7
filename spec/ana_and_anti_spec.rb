967f03193e8f55e22ae87deb92f3ded27707ef3a
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-05-10 17:59:58 -0700

    reword sixth test to reflect array comparison event in multiple_words() method.

--- spec/ana_and_anti_spec.rb
+++ spec/ana_and_anti_spec.rb
@@ -3,32 +3,32 @@ require('ana_and_anti')
 require('pry')
 
 describe ("#ana_and_anti") do
-  # it('the program checks for vowels in the two words') do
-  #   theirWords = Word.new('tea', 'eat')
-  #   expect(theirWords.vowel_checker()).to(eq(true))
-  # end
+  it('the program checks for vowels in the two words') do
+    theirWords = Word.new('tea', 'eat')
+    expect(theirWords.vowel_checker()).to(eq(true))
+  end
   
-  # it('the program will return a confirmation if the words are anagrams') do
-  #   theirWords = Word.new('tea', 'eat')
-  #   expect(theirWords.anagram()).to(eq("These words are an anagram"))
-  # end
+  it('the program will return a confirmation if the words are anagrams') do
+    theirWords = Word.new('tea', 'eat')
+    expect(theirWords.anagram()).to(eq("These words are an anagram"))
+  end
 
-  # it('the program will return a confirmation if the words are ANTIGRAMS') do
-  #   theirWords = Word.new('sup', 'hello')
-  #   expect(theirWords.anagram()).to(eq("This is an antigram"))
-  # end
+  it('the program will return a confirmation if the words are ANTIGRAMS') do
+    theirWords = Word.new('sup', 'hello')
+    expect(theirWords.anagram()).to(eq("This is an antigram"))
+  end
 
-  # it('the program will produce an error statement if there are no actual word inputs') do
-  #   theirWords = Word.new('spd', 'tnl')
-  #   expect(theirWords.anagram()).to(eq("Please input an actual word!"))
-  # end
+  it('the program will produce an error statement if there are no actual word inputs') do
+    theirWords = Word.new('spd', 'tnl')
+    expect(theirWords.anagram()).to(eq("Please input an actual word!"))
+  end
 
-  # it('the program will check that different cases do not effect anagram check') do
-  #   theirWords = Word.new('Tea', 'Eat')
-  #   expect(theirWords.anagram()).to(eq("These words are an anagram"))
-  # end
+  it('the program will check that different cases do not effect anagram check') do
+    theirWords = Word.new('Tea', 'Eat')
+    expect(theirWords.anagram()).to(eq("These words are an anagram"))
+  end
 
-  it('the program will check multiple words that the user may have entered in both inputs') do
+  it('the program will check multiple words that the user may have entered in both inputs for difference in length, and compare arrays accordingly') do
     theirWords = Word.new('tea, stop, bread', 'eat, pots')
     expect(theirWords.multiple_words()).to(eq(["tea", "", "stop", "", "bread"]))
   end
